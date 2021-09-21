eb73d6d4902a5f4b668b9515a02bf3dc4b8eaf54
Gautam Prakash <gautamp@reapbenefit.org>
2021-09-21 13:42:55 +0530

    Fixed errors

--- config.js
+++ config.js
@@ -5,48 +5,41 @@ const config = {
   CSV: "https://docs.google.com/spreadsheets/d/1NWpae4UxuLyMKLVzD-O4QSvgoA1DUbMzcNVE6pIrBmY/gviz/tq?tqx=out:csv&sheet=Bagalkot",
   center: [77.5905634, 12.9244925],
   zoom: 10,
-  title: "Neighbourhood Dashboard",
+  title: "City Dashboard",
   description:
-    "Everything in your neighbourhood in one place!",
-  sideBarInfo: ["ShopName", "Pincode", "Description", "Tags"],
+    "Everything in your neighbourhood in one place for the city of Bengaluru!",
+  sideBarInfo: ["ShopName", "Description", "Phone"],
   popupInfo: ["ShopName"],
   filters: [
+
     {
-      zoom: 12,
-      title: "City Dashboard",
-      description:
-        "Everything in your neighbourhood in one place for the city of Bengaluru!",
-      sideBarInfo: ["ShopName", "Pincode", "Description", "Tags"],
-      popupInfo: ["ShopName"],
-      filters: [
-        {
-          type: "dropdown",
-          title: "Category: ",
-          columnHeader: "Category",
-          listItems: [
-            "Govt Services",
-            "Environment-Civic Data"
-          ],
-        },
-        {
-          type: "dropdown",
-          title: "Sub Category: ",
-          columnHeader: "SubCategory",
-          listItems: [
-            "Others",
-            "Defence Hospitals",
-            "SDH/CHC",
-            "Corporators",
-            "MLA",
-            "Waste",
-            "Citizen Services",
-            "Electricity",
-            "Campaigns",
-            "Issue Reported",
-            "PHCs",
-            "Volunteers",
-            "AQM"
-          ],
-        }
+      type: "dropdown",
+      title: "Category: ",
+      columnHeader: "Category",
+      listItems: [
+        "Govt Services",
+        "Environment-Civic Data"
       ],
-    };
+    },
+    {
+      type: "dropdown",
+      title: "Sub Category: ",
+      columnHeader: "SubCategory",
+      listItems: [
+        "Others",
+        "Defence Hospitals",
+        "SDH/CHC",
+        "Corporators",
+        "MLA",
+        "Waste",
+        "Citizen Services",
+        "Electricity",
+        "Campaigns",
+        "Issue Reported",
+        "PHCs",
+        "Volunteers",
+        "AQM"
+      ],
+    }
+  ],
+};
