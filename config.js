c2e824ab2339f81213311027c62dad705b8cf924
Gautam Prakash <gautamp@reapbenefit.org>
2022-06-07 10:43:44 +0530

    added address in sidebar

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const config = {
   title: "City Dashboard",
   description:
     "Everything in your neighbourhood in one place for the city of Bengaluru!",
-  sideBarInfo: ["ShopName", "Description"],
+  sideBarInfo: ["ShopName", "Address", "Description"],
   popupInfo: ["Tags"],
   filters: [
 
